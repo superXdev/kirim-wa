b6c3605b39f9a9db082702850ffc5ff2700d336a
Fikri <fikrizhanking@gmail.com>
2023-07-19 12:08:54 +0700

    Add import excel function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 
-const sentElement = (nomor, pesan) => {
+let otherVal = [];
+
+const sentElement = (nomor, pesan, opt) => {
+	let msg = pesan;
+	for(let i = 0; i < opt.length; i++) {
+		const mark = i + 1;
+		msg = msg.replaceAll('$'+mark.toString(), opt[i]);
+	}
+
 	return `
 		<div class="card mb-2">
 			<div class="card-body p-1">
@@ -7,7 +15,7 @@ const sentElement = (nomor, pesan) => {
 					<div><b>${nomor}</b></div>
 					<div>
 						<a class="btn btn-sm btn-success" 
-						href="whatsapp://send/?phone=${nomor}&text=${pesan}&type=phone_number&app_absent=0">
+						href="whatsapp://send/?phone=${nomor}&text=${msg}&type=phone_number&app_absent=0">
 						<i class="fas fa-paper-plane"></i>
 						</a>
 					</div>
@@ -20,7 +28,7 @@ const sentElement = (nomor, pesan) => {
 $('#single').click(function() {
 	let nomor = $('[name="nomor"]').val();
 	nomor = (nomor[0] == '0') ? '62' + nomor.substr(1) : nomor;
-	const pesan = $('[name="pesan"]').val();
+	const pesan = encodeURI($('[name="pesan"]').val());
 
 	window.location.href = `whatsapp://send/?phone=${nomor}&text=${pesan}&type=phone_number&app_absent=0`
 });
@@ -28,13 +36,61 @@ $('#single').click(function() {
 $('#multi').click(function() {
 	let nomor = $('[name="list-nomor"]').val();
 	nomor = nomor.split('\n');
-	const pesan = $('[name="pesan_multi"]').val();
+	const pesan = encodeURI($('[name="pesan_multi"]').val());
 
 	let el = '';
-	nomor.forEach(val => {
+	nomor.forEach((val, i) => {
 		const no = (val[0] == '0') ? '62' + val.substr(1) : val;
-		el += sentElement(no, pesan);
+		el += sentElement(no, pesan, otherVal[i]);
 	});
 
 	$('#target').html(el);
+});
+
+const ExcelToJSON = function() {
+	this.parseExcel = function(file) {
+		const reader = new FileReader();
+
+		reader.onload = function(e) {
+			const data = e.target.result;
+			const workbook = XLSX.read(data, {
+				type: 'binary'
+			});
+
+			workbook.SheetNames.forEach(function(sheetName) {
+				const XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+				const numberList = JSON.parse(JSON.stringify(XL_row_object));
+
+				let numbers = [];
+				for (i = 0; i < numberList.length; i++) {
+					const columns = Object.values(numberList[i])
+					numbers.push(columns[0]);
+
+					let values = [];
+					columns.forEach((val, i) => {
+						if(i != 0) {
+							values.push(val);
+						}
+					});
+
+					otherVal.push(values);
+				}
+				// console.log(otherVal)
+				$('[name="list-nomor"]').val(numbers.join('\n'));
+			});
+		}
+
+		reader.onerror = function(ex) {
+			console.log(ex);
+		}
+
+		reader.readAsBinaryString(file);
+	}
+}
+
+$('#excelFile').change(function (e) {
+	otherVal = [];
+	const files = e.target.files;
+	const xl2json = new ExcelToJSON();
+	xl2json.parseExcel(files[0]);
 });
